1477fd1d4a6b4815908f5c7ba6c02988cbb0d627
lioneraV2002 <kamyar_maham1381@yahoo.com>
2021-12-02 18:30:45 +0330

    some changes in user class temporarily , including setters and getters

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -2,27 +2,32 @@ package model;
 
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class User
 {
     private String username;
     private String password;
     private String email;
-    private boolean isloggedIn;
+    private boolean isLoggedIn;
     private String role;
     private int score;
     private ArrayList<String> teamNames;
     private ArrayList<String> joinedTeams;
     private ArrayList<Integer>tasks;
+    private final HashMap<String, User> allUsers = new HashMap<>();
 
 
+    public User(){
 
+    }
 
-
-
-    private User(String password, String username, String email)
+    public User(String password, String username, String email)
     {
-
+        setUsername(username);
+        setPassword(password);
+        setEmail(email);
+        allUsers.put(username, this);
     }
 
     private User(String password)
@@ -31,15 +36,79 @@ public class User
     }
 
 
-    private  boolean validatePass(String password)
-    {
-        return false;
+    public HashMap<String, User> getAllUsers() {
+        return allUsers;
     }
 
-
-    private  boolean validateUserName(String username)
-    {
-        return false;
+    public String getUsername() {
+        return username;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isLoggedIn() {
+        return isLoggedIn;
+    }
+
+    public void setLoggedIn(boolean loggedIn) {
+        isLoggedIn = loggedIn;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public ArrayList<String> getTeamNames() {
+        return teamNames;
+    }
+
+    public void setTeamNames(ArrayList<String> teamNames) {
+        this.teamNames = teamNames;
+    }
+
+    public ArrayList<String> getJoinedTeams() {
+        return joinedTeams;
+    }
+
+    public void setJoinedTeams(ArrayList<String> joinedTeams) {
+        this.joinedTeams = joinedTeams;
+    }
+
+    public ArrayList<Integer> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(ArrayList<Integer> tasks) {
+        this.tasks = tasks;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
