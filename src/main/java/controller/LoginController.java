66e31070af535aef205da0ee815c5923723e5b51
mahnooshr <74074818+mahnooshr@users.noreply.github.com>
2021-12-04 15:20:32 +0330

    Update LoginController.java

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -1,1 +1,29 @@
 
+package controller;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+public class Controller {
+   public boolean loginuser(String username, String password) {
+        User activeUser = findLoggedInUser();
+        if (activeUser != null) {
+            System.out.println("You must logout first!!!");
+            return false;
+        } else {
+            for (User user : User.getAllUsers().keySet()) {
+                if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
+                    activeUser = user;
+                }
+            }
+            if (activeUser == null) {
+                System.out.println("Username or password is wrong!!!");
+                return false;
+            }
+            else {
+                User.getAllUsers().replace(activeUser, true);
+                System.out.println("" + activeUser.getUsername() + " has logged in successfully");
+                return true;
+            }
+        }
+    }}
