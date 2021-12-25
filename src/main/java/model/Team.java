1e5e8ceb655619c09ed4d2e41d39a6174c2b4056
lioneraV2002 <kamyar_maham1381@yahoo.com>
2021-12-25 21:03:15 +0330

    setter getters added

--- src/main/java/model/Team.java
+++ src/main/java/model/Team.java
@@ -1,16 +1,13 @@
 package model;
-
-import view.RoadMapsView;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Team {
-    private static HashMap<String,Team>teams = new HashMap<>();
+    private static final HashMap<String,Team>teams = new HashMap<>();
     private String name;
     private String leader;
     private final ArrayList<String>membersAsideFromLeader = new ArrayList<>();
-    private final ArrayList<String>teamTasks = new ArrayList<>();
+    private final ArrayList<Task>teamTasks = new ArrayList<>();
     private Board board;
     private Chatroom chatroom;
     private RoadMap roadMap;
@@ -21,8 +18,55 @@ public class Team {
     }
 
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public void setLeader(String leader) {
+        this.leader = leader;
+    }
 
+    public void setBoard(Board board) {
+        this.board = board;
+    }
 
+    public void setChatroom(Chatroom chatroom) {
+        this.chatroom = chatroom;
+    }
 
+    public void setRoadMap(RoadMap roadMap) {
+        this.roadMap = roadMap;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLeader() {
+        return leader;
+    }
+
+    public ArrayList<Task> getTeamTasks() {
+        return teamTasks;
+    }
+
+    public Chatroom getChatroom() {
+        return chatroom;
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public RoadMap getRoadMap() {
+        return roadMap;
+    }
+
+    public static HashMap<String, Team> getTeams() {
+        return teams;
+    }
+
+    public ArrayList<String> getMembersAsideFromLeader() {
+        return membersAsideFromLeader;
+    }
 }
