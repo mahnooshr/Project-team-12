bbf7ee77dd608416f025d662120331669979f07a
lioneraV2002 <kamyar_maham1381@yahoo.com>
2021-12-25 21:06:26 +0330

    show board mehtod

--- src/main/java/model/Board.java
+++ src/main/java/model/Board.java
@@ -1,4 +1,58 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+
 public class Board {
+    int phase;
+    String name;
+    String teamName;
+    String leader;
+    HashMap<String, ArrayList<String>>categoriesAndTasks = new HashMap<>();
+    ArrayList<Task>tasks;
+    HashMap<String, Board>boards = new HashMap<>();
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPhase() {
+        return phase;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+
+    public String getLeader() {
+        return leader;
+    }
+
+    @Override
+    public String toString() {
+
+        return ("Board name:" + getName() +
+                "\nBoard completion:" + getDonePercentage() +
+                "\nBoard failed:" + getFailedPercentage() +
+                "\nBoard leader:" + getLeader() +
+                "\nBoard tasks:\n" + showTasks());
+    }
+
+    private float getFailedPercentage() {
+        return (float) (categoriesAndTasks.get("failed").size()/categoriesAndTasks.size());
+    }
+
+    private float getDonePercentage() {
+        return (float)(categoriesAndTasks.get("done").size()/ categoriesAndTasks.size());
+    }
+
+
+
+    public String showTasks() {
+        ArrayList<Task> allTasks = new ArrayList<>(tasks);
+        return Task.sortAndShow(tasks);
+    }
+
 }
