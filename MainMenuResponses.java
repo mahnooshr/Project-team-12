6abb03a3d0325dceab825772f2d0af83317e418c
mahnooshr <74074818+mahnooshr@users.noreply.github.com>
2022-01-15 22:46:24 +0330

    Add files via upload

---
+++ MainMenuResponses.java
@@ -0,0 +1,10 @@
+package controller.responses;
+
+public enum MainMenuResponses {
+    USER_LOGOUT_SUCCESSFUL,
+    SHOW_MENU,
+    MENU_ENTER_SUCCESSFUL,
+    MENU_NAVIGATION_NOT_POSSIBLE,
+    MENU_NAME_NOT_EXIST,
+    ENTER_SUCCESSFULLY;
+}
---
+++ TaskPageResponses.java
@@ -0,0 +1,4 @@
+package controller.responses;
+
+public class TaskPageResponses {
+}
