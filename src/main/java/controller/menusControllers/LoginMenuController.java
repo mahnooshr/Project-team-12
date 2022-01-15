41d5fc240a7352f1810c2bc965a28fa1be8c54c3
mahnooshr <74074818+mahnooshr@users.noreply.github.com>
2022-01-15 22:34:27 +0330

    Update LoginMenuController.java

--- src/main/java/controller/menusControllers/LoginMenuController.java
+++ src/main/java/controller/menusControllers/LoginMenuController.java
@@ -1,53 +1,73 @@
-package controller.menusControllers;
+package controller.menuController;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-import controller.utilityController.ImportExportController;
-import controller.utilityController.RegexController;
+import controller.responses.LoginMenuResponses;
 import model.User;
 
-public class LoginMenuController extends MenuController {
-    public LoginMenuController() {
-        super("login menu");
+public class LoginController extends MenuController {
+    public LoginController() {
+        super("Login Menu");
     }
-
-    public String register(String username, String password1, String password2, String email) {
-        if (!User.doesUsernameExist(username))
-            return "username with name " + username + " already exists";
-        else if (!password1.equals(password2))
-            return "passwords are not the same";
-        else if (RegexController.isPasswordValid(password1))
-            return "password is not strong";
-        else if (!RegexController.isEmailValid(email))
-            return "email is not valid";
-        else if (!User.emailDoesNotExist(email)) {
-            return "email already exists";
-        } else if (username.length() <= 4) {
-            return "your username must contain more than 4 letters";
-        } else {
-            new User(username, password1, email);
-            return "registered successfully";
-        }
-    }
-
-    public String loginUser(String username, String password) {
-        User user = User.getUserByUsername(username);
-        if (user == null || !user.getPassword().equals(password))
-            return "username or password didn't match";
+public LoginMenuResponses welcom()
+{
+    return LoginMenuResponses.WELCOM;
+}
+    public LoginMenuResponses registerUser(String userName, String passWord , String passWord2,String Email) {
+        databaseController.refreshUsersFromFileJson();
+        if (User.getUserByUserName(userName) != null)
+            return LoginMenuResponses.USER_USERNAME_ALREADY_EXISTS;
+        else if (!isPasssame(passWord,passWord2))
+            return LoginMenuResponses.PASS_NOT_SAME;
+        else if (User.getUserByEmail(Email) != null)
+            return LoginMenuResponses.USER_EMAIL_ALREADY_EXISTS;
+        else if (!isEmailValid(Email))
+            return LoginMenuResponses.EMAIL_IS_INVALID;
         else {
-            MenuController.getInstance().setActiveUser(user);
-            return "login successfully";
+            User.getAllUsers().add(new User(userName,passWord,Email, User.UserType.MEMBER));
+            databaseController.refreshUsersToFileJson();
+            return LoginMenuResponses.USER_CREATE_SUCCESSFUL;
         }
     }
+public boolean isPasssame(String password1,String password2)
+{
+    if(password1.equals(password2))
+        return true;
+    else
+        return false;
+}
+public boolean isEmailValid(String Email)
+{
+    if(Email.matches("[A-Za-z0-9.]+@(yahoo.com|gmail.com)"))
+    return true;
+    else
+    return false;
+}
+    public LoginMenuResponses loginUser(String username, String password) {
+        databaseController.refreshUsersFromFileJson();
+        if (User.getUserByUserName(username) == null) {
+            return LoginMenuResponses.IS_NOT_USER;
+            }
+        else if(!User.getUserByUserName(username).getPassWord().equals(password))
 
-    public String removeUser(String username, String password) {
-        User user = User.getUserByUsername(username);
-        if (user == null || !user.getPassword().equals(password))
-            return "username or password invalid";
+    {
+        return LoginMenuResponses.USER_USERNAME_PASSWORD_NOT_MATCHED;
+    }
         else {
-            User.removeByUserName(username);
-            ImportExportController.getInstance().refreshUsersToFileJson();
-            return "user deleted successfully";
+            MenuController.setUser(User.getUserByUserName(username));
+            return LoginMenuResponses.USER_LOGIN_SUCCESSFUL;
         }
     }
 
-
+    public LoginMenuResponses removeUser(String username, String password) {
+        databaseController.refreshUsersFromFileJson();
+        if (User.getUserByUserName(username) == null) return LoginMenuResponses.USER_USERNAME_PASSWORD_NOT_MATCHED;
+        else if (!User.getUserByUserName(username).getPassWord().equals(password))
+            return LoginMenuResponses.USER_USERNAME_PASSWORD_NOT_MATCHED;
+        else {
+            User.removeUserByUsername(username);
+            databaseController.refreshUsersToFileJson();
+            return LoginMenuResponses.USER_REMOVE_SUCCESSFUL;
+        }
+    }
 }
